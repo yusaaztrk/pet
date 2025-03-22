f0653a31127d241f4a19d697583200369cc10400
sametbassoy <samet_bassoy@hotmail.com>
2025-03-23 02:23:32 +0300

    yeni app

--- App.tsx
+++ App.tsx
@@ -91,7 +91,7 @@ function App(): React.JSX.Element {
           }}>
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen22 and then come back to see your edits.
+            screen and then come back to see your edits.
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
@@ -115,7 +115,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 24,
   },
   sectionTitle: {
-    fontSize: 26,
+    fontSize: 24,
     fontWeight: '600',
   },
   sectionDescription: {
